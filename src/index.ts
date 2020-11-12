86fe2bbf53873c8834a3f9e5ba0284541f8a4a0e
abdbuzurg <thesarboz@gmail.com>
2020-11-12 12:16:54 +0500

    added index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("adjg;kjdlgjk ds fg");
+import { MikroORM } from '@mikro-orm/core';
+
+( async() => {
+  const orm = await MikroORM.init({
+    entities: [],
+    dbName: "final-year-project",
+    type: "mysql",
+    debug: true,
+  });
+});
