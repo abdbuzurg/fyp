410c918f5c0a0d5328964c5c44e35987955fcd5e
abdbuzurg <thesarboz@gmail.com>
2020-11-16 20:59:07 +0500

    optimization and encapsulation of env variables

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,9 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 export const COOKIE_NAME = "qid";
+export const DB_NAME = process.env.DB_NAME;
+export const DB_USERNAME = process.env.DB_USERNAME;
+export const DB_PASSWORD = process.env.DB_PASSWORD;
+export const __production__ = (process.env.PRODUCTION === "production");
--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,6 +1,7 @@
 import { MikroORM } from '@mikro-orm/core'
 import path from "path";
 import User from './entities/User'; 
+import { DB_NAME, DB_PASSWORD, DB_USERNAME, __production__ } from './constants';
 
 export default {
   entities: [User],
@@ -8,9 +9,9 @@ export default {
     path: path.join(__dirname, './migrations'), 
     pattern: /^[\w-]+\d+\.[tj]s$/, 
   },
-  dbName: "final-year-project",
+  dbName: DB_NAME,
   type: "mysql",
-  password: "password",
-  user: "root",
-  debug: !process.env.production,
+  password: DB_PASSWORD,
+  user: DB_USERNAME,
+  debug: __production__,
 } as Parameters<typeof MikroORM.init>[0];
